421054b74c072c6a8879f031428bcd2d080c00f3
apavlad <apavlad@rambler.ru>
2019-10-16 15:04:52 +0300

    Ипотека.[#177672]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -1,20 +1,18 @@
 package ru.job4j.loop;
 
 public class Mortgage {
-    public int year(int amount, int salary, double percent) {
+    public static int year(int amount, int salary, double percent) {
         int year = 0;
-        int a = amount;
-        int s = salary;
-        double p = percent;
         while (amount > 0) {
-            amount = (int) (a + (a / 100 * p) - s);
-            if (amount > 0) {
-                year++;
-                return amount;
+            amount = (int) (amount + (amount / 100 * percent) - salary);
+            year++;
             }
-
-        }
         return year;
+        }
+
     }
-}
+
+
+
+
 
