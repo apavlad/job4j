1dfd103767a45ca306daae7cc06a3a93ec120442
apavlad <apavlad@rambler.ru>
2019-10-21 23:02:20 +0300

    6.1. Классический поиск перебором тест.[#177677]

---
+++ chapter_001/src/test/java/array/FindLoopTest.java
@@ -0,0 +1,20 @@
+package array;
+
+import org.junit.Test;
+import ru.job4j.array.FindLoop;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+}
