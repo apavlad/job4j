386a5d358e38ca1cced2ef95f4a7376a4b20ff7e
apavlad <apavlad@rambler.ru>
2019-11-06 19:15:11 +0300

    1. Экзамен. Базовый синтаксис.[#177714]

--- chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -7,30 +7,24 @@ public class Merge {
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         int i = 0, j = 0, k = 0;
-        while (i < left.length && j < right.length)
-        {
-            if (left[i] < right[j])
-            {
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
                 rsl[k] = left[i];
                 i++;
-            }
-            else
-            {
+            } else {
                 rsl[k] = right[j];
                 j++;
             }
             k++;
         }
 
-        while (i < left.length)
-        {
+        while (i < left.length) {
             rsl[k] = left[i];
             i++;
             k++;
         }
 
-        while (j < right.length)
-        {
+        while (j < right.length) {
             rsl[k] = right[j];
             j++;
             k++;
