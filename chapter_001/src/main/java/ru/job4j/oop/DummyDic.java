ae24d6ece08226caf882ef8c58f8f9bfdb1db681
apavlad <apavlad@rambler.ru>
2019-11-29 10:11:32 +0300

    1.4. Переводчик Русский-Английский правильный[#177800]

--- chapter_001/src/main/java/ru/job4j/oop/DummyDic.java
+++ chapter_001/src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,12 +2,12 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String world) {
-        world = "Неизвестное слово";
-        return world;
+        return "Неизвестное слово " + world;
     }
+
     public static void main(String[] args) {
         DummyDic dic = new DummyDic();
         String eng = dic.engToRus("kuku");
-        System.out.println(eng + " - kuku");
+        System.out.println(eng);
     }
 }
