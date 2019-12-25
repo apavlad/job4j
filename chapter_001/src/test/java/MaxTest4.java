72e0a49998ee18698461bce1765e32a06936d14e
apavlad <apavlad@rambler.ru>
2019-12-25 17:02:17 +0300

    3. Перегрузить метод max для трех чисел test[#177793]

--- chapter_001/src/test/java/MaxTest4.java
+++ chapter_001/src/test/java/MaxTest4.java
@@ -6,20 +6,20 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest4 {
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMax1To2() {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMax2To1Then2() {
-        int result = Max.max(2, 1);
-        assertThat(result, is(2));
+    public void whenMax1To3() {
+        int result = Max.max(2, 1, 5);
+        assertThat(result, is(5));
     }
 
     @Test
-    public void whenMax2To2Then2() {
-        int result = Max.max(2, 2);
-        assertThat(result, is(2));
+    public void whenMax1T4() {
+        int result = Max.max(2, 2, 5, 4);
+        assertThat(result, is(5));
     }
 }
