d94f6847afe548a0a50a1f161641ef40ffeeaaa8
apavlad <apavlad@rambler.ru>
2020-03-02 16:04:14 +0300

    0. Видео[#177811]

--- chapter_0002/src/main/java/Inheritance/Config.java
+++ chapter_0002/src/main/java/Inheritance/Config.java
@@ -3,6 +3,7 @@ package Inheritance;
 public class Config {
     private String name;
     private int position;
+
     private String[] properties;
     public Config(String name) {
         this.name = name;
