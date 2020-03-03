418a78c36b08c66c964d4302ca4f2086786372d0
apavlad <apavlad@rambler.ru>
2020-03-03 11:24:46 +0300

    2. Модель данных.[#177813]

---
+++ chapter_0002/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Ivan Ivanov");
+        student.setGroup("12A");
+        student.setReceipt(new Date());
+
+        System.out.println(student.getFio() + "\n" + student.getGroup() + "\n" + student.getReceipt() );
+
+    }
+}
---
+++ chapter_0002/src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String fio;
+    private String group;
+    private Date receipt;
+
+        public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setReceipt(Date receipt) {
+        this.receipt = receipt;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public Date getReceipt() {
+        return receipt;
+    }
+
+
+}
