1b7e5eab6ac0d68bbb3c932d52f20bb001b031aa
apavlad <apavlad@rambler.ru>
2020-03-12 14:18:30 +0300

    3. Массивы и модели.[#177814]

--- chapter_0002/src/main/java/ru/job4j/pojo/Book.java
+++ chapter_0002/src/main/java/ru/job4j/pojo/Book.java
@@ -3,24 +3,19 @@ package ru.job4j.pojo;
 public class Book {
     private String name;
     private int pages;
-
     public Book(String name, int pages) {
         this.name = name;
         this.pages = pages;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getPages() {
         return pages;
     }
-
     public void setPages(int pages) {
         this.pages = pages;
     }
--- chapter_0002/src/main/java/ru/job4j/pojo/Library.java
+++ chapter_0002/src/main/java/ru/job4j/pojo/Library.java
@@ -6,13 +6,11 @@ public class Library {
         Book b2 = new Book("Grachy", 114);
         Book b3 = new Book("Utro", 192);
         Book b4 = new Book("Clean code", 202);
-
         Book[] books = new Book[4];
         books[0] = b1;
         books[1] = b2;
         books[2] = b3;
         books[3] = b4;
-
         for (int index = 0; index < books.length; index++) {
             Book bk = books[index];
             System.out.println(bk.getName() + " - " + bk.getPages());
@@ -28,7 +26,5 @@ public class Library {
         if(books[0].equals(tmp)) {
             System.out.println(tmp.getName());
         }
-
-
     }
 }
