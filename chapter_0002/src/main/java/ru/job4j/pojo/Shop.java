4bb3a66f49b82aa99ce2428314c4d60996c1b3c9
apavlad <apavlad@rambler.ru>
2020-04-02 13:22:54 +0300

    3. Удаление моделей из массива.[#177816]

---
+++ chapter_0002/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,57 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        //удаляем значение из ячейки с индексом 1
+        products[1] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        public Product[] delete(Product[] int index) {
+            for (index = 0; index < products.length; index++) {
+                products[index] = products[index + 1];
+                if (products != null) {
+                    System.out.println(products.getName());
+                } else {
+                    System.out.println("null");
+                }
+            }
+            products[products.lenght - 1] = null;
+        }
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+}
